1e59c87284d07eb6ff46958a6294d45153d4f3e4
Illak Zapata <i.zapata1989@gmail.com>
2024-04-26 13:30:24 -0300

    update

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 from PyPDF2 import PdfReader
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
@@ -11,6 +14,17 @@ from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 from langchain_core.output_parsers import StrOutputParser
+
+from google.generativeai.types.safety_types import HarmBlockThreshold, HarmCategory
+
+SAFETY_SETTINGS = {
+    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE, 
+    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE, 
+    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+}
+
+import re
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
@@ -25,7 +39,35 @@ def get_pdf_text(pdf_docs):
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
+
+            # Limpieza
+            text = re.sub(r" +", " ", text)
+            text = re.sub(r'\n', ' ', text)
+
     return text
+
+def load_pdf_text():
+    text = ""
+
+    directory = 'libros_pyc'
+ 
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print(f)
+            pdf_reader = PdfReader(f)
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+
+                # Limpieza
+                text = re.sub(r" +", " ", text)
+                text = re.sub(r'\n', ' ', text)
+
+    return text
+
 
 # split text into chunks
 
@@ -49,22 +91,16 @@ def get_vector_store(chunks):
 def get_conversational_chain():
 
     prompt_template = """
-    Basándote en el siguiente contexto:\n\n
-
-    ###
-    A partir del siguiente contexto:\n\n
-
-    -------------------------------\n
-    Contexto:\n {context}\n
-    -------------------------------\n
-
     Responder a la pregunta del usuario lo más detallademente posible. Si la respuesta no se encuentra
-    en el contexto provisto responder: "Lo siento, no tengo información al respecto...", no devuelva una respuesta incorrecta\n\n
-    Si encuentra la respuesta en el contexto provisto, además puede agregar a la respuesta información externa que contribuya
-    o deje una reflexión sobre la respuesta encontrada.\n
+    en el contexto provisto responder: "Lo siento, no tengo información al respecto...", no devuelva una respuesta incorrecta.
+    Si encuentra la respuesta en el contexto provisto, agregar información externa, que no esté en el contexto, que contribuya
+    o deje una reflexión sobre la respuesta encontrada en caso de que sea posible.\n
 
+    -------------------------------\n
+    Contexto: {context}\n
+    -------------------------------\n
 
-    Pregunta del usuario:\n {question}\n
+    Pregunta del usuario: {question}\n
 
     Respuesta:
     """
@@ -72,12 +108,16 @@ def get_conversational_chain():
     model = ChatGoogleGenerativeAI(model="gemini-pro",
                                    client=genai,
                                    temperature=0.3,
+                                   maxOutputTokens=2000,
+                                   safety_settings=SAFETY_SETTINGS
                                    )
+    
     prompt = PromptTemplate(template=prompt_template,
                             input_variables=["context", "question"])
-    #chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
 
-    chain = prompt | model | StrOutputParser()
+    #chain = prompt | model | StrOutputParser()
 
     return chain
 
@@ -92,6 +132,7 @@ def user_input(user_question):
         model="models/embedding-001")  # type: ignore
 
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    
     docs = new_db.similarity_search(user_question)
 
     print(len(docs))
@@ -101,23 +142,38 @@ def user_input(user_question):
 
     chain = get_conversational_chain()
     
-    response = chain.stream({
-            "context": docs, 
-            "question": user_question
-         })
+    #response = chain.stream({
+    #        "context": docs, 
+    #        "question": user_question
+    #     })
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, return_only_outputs=True, )
 
     print(response)
     return response
 
+@st.cache_resource
+def initialize_vector_db():
+    raw_text = load_pdf_text()
+    text_chunks = get_text_chunks(raw_text)
+    get_vector_store(text_chunks)
+    st.success("Listo!")
 
-def main():
+
+def main(): 
     st.set_page_config(
         page_title="Gemini PDF Chatbot",
         page_icon="🤖"
     )
 
+    # Cargamos los libros de PYC al INICIO
+    with st.spinner("Procesando PDFs..."):
+        initialize_vector_db()
+
+
     # Sidebar for uploading PDF files
-    with st.sidebar:
+    _ = """with st.sidebar:
         st.title("Menu:")
         pdf_docs = st.file_uploader(
             "Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
@@ -126,19 +182,20 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
                 text_chunks = get_text_chunks(raw_text)
                 get_vector_store(text_chunks)
-                st.success("Done")
+                st.success("Done")"""
+
 
     # Main content area for displaying chat messages
-    st.title("Chateando con contenido de PDFs (ISEP) 🤖")
+    st.title("Chateando con los libros de Pedagogía y Cultura (ISEP) 🤖")
     st.write("Bienvenido al chat!")
-    st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+    st.sidebar.button('Limpiar chat', on_click=clear_chat_history)
 
     # Chat input
     # Placeholder for chat messages
 
     if "messages" not in st.session_state.keys():
         st.session_state.messages = [
-            {"role": "assistant", "content": "Escriba preguntas sobre el MI"}]
+            {"role": "assistant", "content": "Escriba preguntas sobre el documento"}]
 
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -153,10 +210,17 @@ def main():
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
             with st.spinner("Estoy pensando..."):
-                response = st.write_stream(user_input(prompt))
+                #response = st.write_stream(user_input(prompt))
+                response = user_input(prompt)
+                placeholder = st.empty()
+                full_response = ''
+                for item in response['output_text']:
+                    full_response += item
+                    placeholder.markdown(full_response)
+                placeholder.markdown(full_response)
                 
         if response is not None:
-            message = {"role": "assistant", "content": response}
+            message = {"role": "assistant", "content": full_response}
             st.session_state.messages.append(message)
 
 
